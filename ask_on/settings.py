b0566d4a48125a8176456e5c8f83aea2b6bf5313
Ayush Kothari <aayushkothari11@yahoo.com>
2018-01-28 09:17:52 +0530

    Login view

--- ask_on/settings.py
+++ ask_on/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'forum',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.urls import reverse
 
-# Create your views here.
+
+def login(request):
+    if request.user.is_authenticated:
+            return render(request, 'forum/feed.html', {})
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username', '')
+            password = request.POST.get('password', '')
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    auth_login(request, user)
+                    return render(request, 'forum/feed.html', {})
+                else:
+                    error = 'Your account is disabled.'
+                    return render(request, 'forum/login.html', {'error': error})
+            else:
+                error = 'Incorrect Username or Password'
+                return render(request, 'forum/login.html', {'error': error})
+        else:
+            return render(request, 'forum/login.html', {})
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect(reverse('login'))
